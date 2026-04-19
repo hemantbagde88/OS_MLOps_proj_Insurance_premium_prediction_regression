cc60e661b555d175e51815ce75cbb3ea297e2c7b
Hemant <hemantbagde88@gmail.com>
2026-04-19 23:39:02 +0530

    template is ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Hemant Bagde",
+    author_email="hemantbagde88@gmail.com",
+    packages=find_packages()
+)
